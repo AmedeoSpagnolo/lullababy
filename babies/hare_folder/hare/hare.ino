5f3a9143c911e1d7f68e77924fe4f43a07c77def
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2019-09-23 16:13:38 +0900

    add sound behavior to state machine

--- babies/hare_folder/hare/hare.ino
+++ babies/hare_folder/hare/hare.ino
@@ -1,40 +1,57 @@
-enum State_enum {SILENT,CALM,CRY};
-enum Sensors_enum {NONE, ACC};
+#include <SD.h>
+#include <SPI.h>
+#include <arduino.h>
+#include <MusicPlayer.h>
+
+enum state_enum {MUTE,CALM,CRY,CALM_MOVING,CRY_MOVING};
+char mp3name[3][30] = {"silence.mp3","light1.mp3","hard1.mp3"};
 
 void state_machine_run();
-void baby_silent();
+void baby_mute();
 void baby_calm();
 void baby_cry();
+bool bigMovement();
+bool isMoving();
 
 int time = 0;
-const int MAX_SILENT        = 34543212;
-const int MAX_CALM          = 34543212;
-const int MAX_CRY           = 34543212;
-const int CALM_MOVING_TIME  = 34543212;
-const int CRY_MOVING_TIME   = 34543212;
+int MAX_MUTE          = 800;
+int MAX_CALM          = 800;
+int MAX_CRY           = 800;
+int CALM_MOVING_TIME  = 800;
+int CRY_MOVING_TIME   = 800;
 
 
-uint8_t state = SILENT;
+uint8_t state = MUTE;
 
 void setup(){
+  Serial.begin(9600);
+  player.begin();
+  player.keyDisable();
+  player.setPlayMode(PM_REPEAT_ONE);
+  player.playOne(mp3name[0]);
 }
 
 void loop(){
   time += 1;
+  Serial.print("State: ");
+  Serial.print(state);
+  Serial.print(" - Time: ");
+  Serial.println(time);
+  player.play();
   state_machine_run();
-  delay(10);
+  delay(20);
 }
 
-void state_machine_run(uint8_t sensors)
+void state_machine_run()
 {
   switch(state)
   {
-    case SILENT:
+    case MUTE:
       if(bigMovement()){
         baby_cry();
         state = CRY;
       }
-      if(time > MAX_SILENT){
+      if(time > MAX_MUTE){
         time = 0;
         baby_calm();
         state = CALM;
@@ -48,7 +65,6 @@ void state_machine_run(uint8_t sensors)
         state = CRY;
       }
       if(isMoving()){
-        baby_calm_moving();
         state = CALM_MOVING;
       }
       if(bigMovement()){
@@ -63,8 +79,8 @@ void state_machine_run(uint8_t sensors)
         state = CALM;
       }
       if(time > CALM_MOVING_TIME){
-        baby_silent();
-        state = SILENT;
+        baby_mute();
+        state = MUTE;
       }
       if(bigMovement()){
         baby_cry();
@@ -75,11 +91,10 @@ void state_machine_run(uint8_t sensors)
     case CRY:
       if(time > MAX_CRY){
         time = 0;
-        baby_silent();
-        state = SILENT;
+        baby_mute();
+        state = MUTE;
       }
       if(isMoving()){
-        baby_calm_moving();
         state = CRY_MOVING;
       }
       break;
@@ -98,24 +113,25 @@ void state_machine_run(uint8_t sensors)
         state = CRY;
       }
       break;
-
   }
 }
 
-void baby_silent()
-{
+void baby_mute(){
+  player.playOne(mp3name[0]);
 }
-void baby_calm()
-{
-  player.play("light1.mp3");
+
+void baby_calm(){
+  player.playOne(mp3name[1]);
 }
-void baby_cry()
-{
-  player.play("heavy1.mp3");
+
+void baby_cry(){
+  player.playOne(mp3name[2]);
 }
-void bigMovement()
-{
+
+bool bigMovement(){
+  return false;
 }
-void isMoving()
-{
+
+bool isMoving(){
+  return false;
 }
