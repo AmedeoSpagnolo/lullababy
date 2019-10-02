e330528bb1141bd6e2d6b54ce43f17a6d774ab84
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2019-10-02 11:38:08 +0900

    separate state machine and add comments

--- babies/kenta_folder/kenta/kenta.ino
+++ babies/kenta_folder/kenta/kenta.ino
@@ -4,54 +4,32 @@
 #include <MusicPlayer.h>
 #include <Math.h>
 #include "param.h"
-
-//functions
-void state_machine();
-void updateTime();
-
-enum state_enum {ASLEEP,AWAKE};
+#include "statemachine.h"
 
 char mp3name[3][15] = {"a.mp3","b.mp3","c.mp3"};
 
-//variables
-//3600 s in an hour 3600000 ms in an hour
-unsigned long current_time;
-unsigned long start_time;
-unsigned long time_elapsed;
+unsigned long birthday = millis();
 
-long prev; //Accelerometer
-
-int millifeels; //new SI metric for feelings
-// general reference range from 0 to 1000.
-// 0 - 250: RAWR
-// 250 - 500: BOO
-// 500 - 750: meh
-// 750 - 1000: Playful
-// 0_______________________________________________1000
-// (ToT)         (T_T)           (;_;)            ('U')
-//unsigned long wakeup_intervals [];
-
-uint8_t state = AWAKE; //initial state
-
-
-//Booleans
-bool isAsleep = false;
-bool DEBUG = false;
-bool ACCELEROMETER_DEBUG = false;
-bool isTimeSleep();
-
+// Accelerometer
+long prev  = 0;
 bool shake = false;
 bool shock = false;
+
+//    New SI metric for feelings
+//    0___________250___________500___________750___________1000
+//        (ToT)         (T_T)         (;_;)         ('U')
+//
+int emotionLevel = 80;
+
+// DEBUG
+bool DEBUG = false;
+bool ACCELEROMETER_DEBUG = false;
 
 void check_physical_love(); //previously bool isMoving()
 void update_feelings(); //use both in awake and asleep
 
 void setup(){
   Serial.begin(9600);
-  current_time = millis(); // set timer
-  start_time = current_time;
-  millifeels = 80; //set state to be 80
-  prev = 0;
 
   //music shield
   player.begin();
@@ -62,100 +40,26 @@ void setup(){
 }
 
 void loop(){
-  updateTime(); // figure out time stamp
   check_physical_love(); //update shake and shock
   update_feelings();
-  state_machine();
+  state_machine_run(birthday);
+  Serial.print("State: ");    Serial.print(state);
+  Serial.print(" - EmotionLevel: "); Serial.print(emotionLevel);
+  Serial.print(" - Shake: "); Serial.print(shake);
+  Serial.print(" - Shock: "); Serial.println(shock);
+  delay(50);
 }
 
-void state_machine_run(){ //Logic for Awake and Asleep states
-  switch(state){
-    case AWAKE:
-      status_awake_loop();
-      if(isTimeToSleep()){
-        status_awake_exit();
-        status_asleep_enter();
-      }
-      break;
-
-    case ASLEEP:
-      status_asleep_loop();
-      if(!isTimeToSleep()){
-        status_asleep_exit();
-        status_awake_enter();
-      }
-      break;
-  }
-}
-
-// Check time and schedule
-//
-void updateTime(){
-  current_time = millis();
-  time_elapsed = current_time - start_time;
-}
-
-bool isTimeToSleep(){
-  return false;
-}
-
-
-// Awake functions
-//
-void status_awake_enter(){
-return true;
-}
-
-void status_awake_loop(){
-  // led pulse normal,
-  // bad - good indicatior results to high - zero volume
-  switch(millifeels){
-    case 0 ... 250:
-      player.setVolume(0x00); // 0
-      break;
-    case 251 ... 500:
-      player.setVolume(0x50); //80
-      break;
-    case 501 ... 750:
-      player.setVolume(0xa0); //160
-      break;
-    case 751 ... 1000:
-      player.setVolume(0xfe); //
-      break;
-  }
-}
-
-void status_awake_exit(){
-return true;
-}
-
-//asleep functions
-
-void status_asleep_enter(){
-return true;
-}
-
-void status_asleep_exit(){
-  return true;
-}
-
-void status_asleep_loop(){
-  // led pulse slow
-  return true;
-}
-
-
-
-//accelerometer. Updates Shock and Shake. prev. isMoving().
+//accelerometer. Updates Shock and Shake.
 void check_physical_love(){
-  int i; //initialize local variables
+  int i;
   long curr;
   long delta;
   long x = 0;
   long y = 0;
   long z = 0;
 
-  //read value 50 times and output the average
+//read value 50 times and output the average
   for (i=0 ; i < 50 ; i++) {
     x = x + analogRead(3);
     y = y + analogRead(4);
@@ -165,7 +69,7 @@ void check_physical_love(){
   curr = x + y + z;
   delta = abs(curr-prev); // compare with last sum(x,y,z)
 
-  //update shake and shock and prev
+//update shake and shock and prev
   shake = (delta > 4);
   shock = (delta > 400);
   prev = curr;
@@ -181,16 +85,16 @@ void check_physical_love(){
 
 void update_feelings(){
   if (shake && !shock){
-    millifeels += 1; //feelings are obviously linear
-    if (millifeels > 999){
-      millifeels = 995;  // i feel like it might get stuck 999
+    emotionLevel += 1; //feelings are obviously linear
+    if (emotionLevel > 999){
+      emotionLevel = 995;  // i feel like it might get stuck 999
     }
   } else if (!shake && !shock){
-    millifeels -= 4;
-    if (millifeels < 0){
-      millifeels = 5; // i feel like it might get stuck if 0
+    emotionLevel -= 4;
+    if (emotionLevel < 0){
+      emotionLevel = 5; // i feel like it might get stuck if 0
     }
   } else if (shock){ //if shock, make it in cry state
-    millifeels = 15;
+    emotionLevel = 15;
   }
 }
---
+++ babies/kenta_folder/kenta/statemachine.h
@@ -0,0 +1,42 @@
+enum state_enum {ASLEEP,AWAKE};
+
+uint8_t state = AWAKE; //initial state
+
+void status_awake_enter(){}
+void status_awake_exit(){}
+void status_awake_loop(){}
+void status_asleep_enter(){}
+void status_asleep_exit(){}
+void status_asleep_loop(){}
+
+bool _isTimeToSleep(unsigned long birthday){
+  long temp = (millis() - birthday) % 30000;
+  if (temp < 1000){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void state_machine_run(unsigned long birthday){
+  bool timetosleep = _isTimeToSleep(birthday);
+  switch(state){
+    case AWAKE:
+      status_awake_loop();
+      if(timetosleep){
+        status_awake_exit();
+        status_asleep_enter();
+        state = ASLEEP;
+      }
+      break;
+
+    case ASLEEP:
+      status_asleep_loop();
+      if(!timetosleep){
+        status_asleep_exit();
+        status_awake_enter();
+        state = AWAKE;
+      }
+      break;
+  }
+}
