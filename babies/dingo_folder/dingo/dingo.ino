48e530bf4855896b471cefdda2ffe95753831a3a
Amedeo Spagnolo <amedeo.spagnolo@gmail.com>
2019-10-04 17:52:47 +0900

    minor fixes

--- babies/dingo_folder/dingo/dingo.ino
+++ babies/dingo_folder/dingo/dingo.ino
@@ -7,7 +7,7 @@ Movement acc = Movement();
 
 void setup(){
   Serial.begin(9600);
-  acc.pins(3,4,6); // x, y, z
+  acc.pins(3,4,5); // x, y, z
   acc.shockSensibility(5); // 0 - 10
   acc.shakeSensibility(5); // 0 - 10
 }
